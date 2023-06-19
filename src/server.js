31811a0801f18adab1724b4a9ee98c37778d12ae
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-06-19 18:56:44 +0900

    Recap and final setup to backend

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ app.set("views", `${process.cwd()}/src/views`);
 
 app.use("/public", express.static(`${process.cwd()}/src/public`));
 
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 app.listen(3000, () => console.log(`Listening on http://localhost:3000`));
