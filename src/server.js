157c9d347e58b09a16fa66a179f2a655c6570869
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-06-19 21:54:46 +0900

    Connecting server & client with WebSocket, and Websocket events

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
+const PORT = 3000;
 
 app.set("view engine", "pug");
 app.set("views", `${process.cwd()}/src/views`);
@@ -10,4 +13,15 @@ app.use("/public", express.static(`${process.cwd()}/src/public`));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-app.listen(3000, () => console.log(`Listening on http://localhost:3000`));
+const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
+
+const server = http.createServer(app);
+const wsServer = new WebSocket.Server({ server });
+
+const onConnection = (socket) => {
+  console.log(socket);
+};
+
+wsServer.on("connection", onConnection);
+
+server.listen(PORT, handleListen);
