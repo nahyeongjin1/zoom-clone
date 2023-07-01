6433f70c8bdfa9b4d3f7ff10e31db10199de275a
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-07-01 22:03:21 +0900

    Room notifications

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -15,11 +15,22 @@ function addMessage(message) {
   ul.appendChild(li);
 }
 
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("input");
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${input.value}`);
+    input.value = "";
+  });
+}
+
 function showRoom() {
   const h3 = room.querySelector("h3");
   h3.innerText = `Room ${roomName}`;
   room.hidden = false;
   welcome.hidden = true;
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit);
 }
 
 function handleRoomSubmit(event) {
@@ -31,6 +42,13 @@ function handleRoomSubmit(event) {
 }
 
 form.addEventListener("submit", handleRoomSubmit);
+
 socket.on("welcome", () => {
   addMessage("Someone joined!");
 });
+
+socket.on("bye", () => {
+  addMessage("Someone lefted...");
+});
+
+socket.on("new_message", addMessage);
